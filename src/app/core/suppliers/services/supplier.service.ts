ad748bb80829cc9565717f2b0ec6c7bfdbe3a3cd
Erik Ciau <erikciau777@icloud.com>
2022-10-24 21:44:08 -0500

    added service creator supplier

--- src/app/core/suppliers/services/supplier.service.ts
+++ src/app/core/suppliers/services/supplier.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, retry } from "rxjs";
-import { environment } from 'src/environments/environment'
+import { environment } from 'src/environments/environment';
 import { ISupplierResponse } from "../interfaces/supplier-response.interface";
 
 @Injectable({ providedIn: 'root' })
@@ -14,6 +14,16 @@ export class SupplierService {
       catchError(error => {
         console.log(error)
         throw error
+      })
+    )
+  }
+
+  supplierCreator(supplier: { name: string, contact: string }) {
+    return this.network.post<void>(`${environment.apiBase}/suppliers`, supplier).pipe(
+      retry(1),
+      catchError(err => {
+        console.log(err)
+        throw err
       })
     )
   }
