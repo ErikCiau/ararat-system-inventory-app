5ad825bb4dd33ee5407a45aa5babd0c865aefcfb
Erik Ciau <erikciau777@icloud.com>
2022-10-28 16:03:21 -0500

    Employee creator component added form and submit function

--- src/app/pages/employee-creator/employees-creator.component.ts
+++ src/app/pages/employee-creator/employees-creator.component.ts
@@ -1,6 +1,19 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
   templateUrl: './employees-creator.component.html'
 })
-export class EmployeeCreatorComponent { }
+export class EmployeeCreatorComponent {
+  public employeeForm = {
+    name: '',
+    lastname: '',
+    contact: '',
+    password: '',
+  }
+
+  public submitEmployeeCreatorForm(form: NgForm) {
+    if (form.invalid) return
+    console.log(form.value)
+  }
+}
